e13652df6e7d4807c251a4fbf4902d7ba7faf714
mcnairjm19 <mcnairjm1993@gmail.com>
2022-04-08 13:53:11 -0400

    images

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,20 +1,16 @@
-import { Card, Button } from 'react-bootstrap';
-import Alley21 from '../Assets/John/alleysign2.jpg'
-import Budget from '../Assets/John/budget-tracker.png'
-import Cocktail from '../Assets/John/cocktail.jpg'
-import Run from '../Assets/John/hero-bg.jpg'
+import { Card } from 'react-bootstrap';
 
 const projects = [
   {
     title: 'Prost 4.0',
     description: 'Cocktail App for local restaurant ALley 26',
-    image: {Alley21},
+    image: 'https://raw.githubusercontent.com/mcnairjm/react-portfolio/main/src/Assets/John/alleysign2.jpg',
     link: 'https://github.com/polizoto/Prost_4.0',
   },
   {
     title: 'Budget Tracker',
     description: 'Budget tracker app utilizing PWA capabilities',
-    image: {Budget},
+    image: 'https://raw.githubusercontent.com/mcnairjm/react-portfolio/main/src/Assets/John/budget-tracker.png',
     link: 'https://github.com/mcnairjm/budget-tracker',
   },
   {
@@ -26,13 +22,13 @@ const projects = [
   {
     title: 'Cocktail Search App',
     description: 'Allows users to search and save cocktails, with a randomizer feature',
-    image: {Cocktail},
+    image: 'https://raw.githubusercontent.com/mcnairjm/react-portfolio/main/src/Assets/John/cocktail.jpg',
     link: 'https://github.com/mcnairjm/cocktail-app',
   },
   {
     title: 'Run Buddy',
     description: 'Static website, with advanced HTML & CSS',
-    image: {Run},
+    image: 'https://raw.githubusercontent.com/mcnairjm/react-portfolio/main/src/Assets/John/hero-bg.jpg',
     link: 'https://github.com/mcnairjm/run-buddy',
   }
 
@@ -40,18 +36,23 @@ const projects = [
 
 const MyProject = () => {
   return (
-    projects.map((project) => (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={project.image} />
-      <Card.Body>
-        <Card.Title>{project.title}</Card.Title>
-        <Card.Text>
-          {project.description}
-        </Card.Text>
-        <a href={project.link} class="btn btn-primary btn-lg active" role="button" aria-pressed="true">View Project</a>
-      </Card.Body>
-    </Card>
-    ))
+    
+    <div class='row'>
+      {projects.map((project) => (
+      <div class='col-sm-3'>
+        <Card style={{ width: '18rem', height: '25rem',  }}>
+          <Card.Img variant="top" src={project.image} />
+          <Card.Body>
+            <Card.Title>{project.title}</Card.Title>
+            <Card.Text>
+              {project.description}
+            </Card.Text>
+            <a href={project.link} class="btn btn-primary btn-lg active" role="button" aria-pressed="true">View Project</a>
+          </Card.Body>
+        </Card>
+      </div>
+    ))};
+    </div>
   )
 }
 export default MyProject;
